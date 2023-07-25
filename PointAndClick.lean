155d2bffbda67cf9c737700bdd44b886bd84824a
Mantas Bakšys <39908973+MantasBaksys@users.noreply.github.com>
2023-07-25 17:22:06 +0100

    init

---
+++ PointAndClick.lean
@@ -0,0 +1,2 @@
+import SelectInsertPanel
+import SelectInsertConvDemo
---
+++ lakefile.lean
@@ -0,0 +1,26 @@
+import Lake
+open Lake DSL
+
+package «PointAndClick» {
+  -- add any package configuration options here
+}
+
+require proofwidgets from git 
+  "https://github.com/EdAyers/ProofWidgets4"@"v0.0.13"
+
+require std from git
+  "https://github.com/leanprover/std4"
+
+
+@[default_target]
+lean_lib «PointAndClick» {
+  -- add any library configuration options here
+}
+
+lean_lib SelectInsertPanel {
+
+}
+
+lean_lib SelectInsertConvDemo {
+  
+}
